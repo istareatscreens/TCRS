327bb5013293b9d0ebc58f093d50d5c948d4da17
istareatscreens <schar138@uottawa.ca>
2021-02-12 20:46:38 -0500

    Modified run scripts, and modified colouring

---
+++ Traffic Citation and Reporting System/TCRS-client/b-sync.ps1
@@ -0,0 +1,3 @@
+browser-sync start `
+            --proxy http://localhost:5000/ `
+            --files '**/*.cshtml, **/*.css, **/*.scss, **/*.js, **/*.htm*' `
--- Traffic Citation and Reporting System/TCRS-client/run-watch.ps1
+++ Traffic Citation and Reporting System/TCRS-client/run-watch.ps1
@@ -5,18 +5,8 @@
 #3. run in powershell
 #npm install -g browser-sync
 
-param($Work)
-
-# restart PowerShell with -noexit, the same script, and 1
-if (!$Work) {
-    powershell -noexit -file $MyInvocation.MyCommand.Path 1
-    return
-}
 
 # now the script does something
 # this script just outputs this
 
-dotnet run watch ; browser-sync start `
-            --proxy http://localhost:5000/ `
-            --files '**/*.cshtml, **/*.css, **/*.js, **/*.htm*'
-             
+dotnet run watch
