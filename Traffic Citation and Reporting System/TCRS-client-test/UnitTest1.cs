c3334a6bad6d678489e15567bba3d2f9d5168715
arncurry30 <ralfonso@algomau.ca>
2021-02-12 22:07:11 -0500

    Initial Unit Test

--- Traffic Citation and Reporting System/TCRS-client-test/UnitTest1.cs
+++ Traffic Citation and Reporting System/TCRS-client-test/UnitTest1.cs
@@ -1,18 +1,36 @@
+using Blazor.Demo.Pages;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using NUnit.Framework;
+using System.Linq;
 
 namespace TCRS_client_test
 {
     public class Tests
     {
+		private Bunit.TestContext testContext;
+        private Mock<ILoginProcessor> loginProcessor;
         [SetUp]
         public void Setup()
         {
+			testContext = new Bunit.TestContext();
+            loginProcessor = new Mock<ILoginProcessor>();
+        }
+		[TearDown]
+        public void Teardown()
+        {
+            testContext.Dispose();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            testContext.Services.AddScoped(x => loginProcessor.Object);
+
+            var component = testContext.RenderComponent<Index>();
+
+            Assert.IsTrue(component.Markup.Contains("<h1>Hello, world!</h1>"));
         }
     }
 }
