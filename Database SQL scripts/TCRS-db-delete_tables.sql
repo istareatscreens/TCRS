82cd1d9485f15fc30707dc7d99098ba41de539fc
istareatscreens <schar138@uottawa.ca>
2021-02-15 02:05:03 -0500

    Split sql table creation script to a delete script and a create table script

---
+++ Database SQL scripts/TCRS-db-delete_tables.sql
@@ -0,0 +1,26 @@
+#Delete tables
+
+#Users
+DROP TABLE Person; #1
+DROP TABLE Client_Admin; #2
+DROP TABLE School_Rep; #3
+DROP TABLE Municipal_Officer; #4
+DROP TABLE Highway_Patrol_Officer; #5
+DROP TABLE Municipality; #6
+DROP TABLE Police_Dept; #7
+DROP TABLE School; #8
+DROP TABLE Course; #9
+DROP TABLE Registration; #10
+DROP TABLE Citation; #11
+DROP TABLE Citation_Type; #12
+DROP TABLE Vehicle_Record; #13
+DROP TABLE Driver_Record; #14
+DROP TABLE Vehicle; #15
+DROP TABLE Licence_Plate; #16
+DROP TABLE Payment; #17
+DROP TABLE Wanted_Vehicle; #18
+DROP TABLE Wanted_Citizen; #19
+DROP TABLE Wanted; #20
+DROP TABLE Citizen; #21
+DROP TABLE Insurer; #22
+DROP TABLE Licence; #23
