f9e3ab4d2ca656113f27801849a32465a361f176
istareatscreens <schar138@uottawa.ca>
2021-03-16 13:37:37 -0400

    Refactored login page to use UserLoginCredentials

--- Traffic Citation and Reporting System/TCRS.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TCRS.Business
-{
-    public class Class1
-    {
-    }
-}
---
+++ Traffic Citation and Reporting System/TCRS.client/Pages/SigninBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using TCRS.Shared.Objects;
+
+namespace TCRS.Client.Pages
+{
+    public class SigninBase: ComponentBase
+    {
+        protected UserLoginCredentials UserCredentials { get; set; } = new UserLoginCredentials();
+
+        protected EditContext EditContext { get; set; }
+
+         protected bool success;
+
+         protected string[] errors = { };
+
+         protected override void OnInitialized()
+         {      
+             base.OnInitialized();
+             EditContext = new EditContext(UserCredentials);
+         }
+
+         protected void Submit()
+        {
+              Console.WriteLine(UserCredentials.Email + " " + UserCredentials.Password);
+        }
+    }
+}
