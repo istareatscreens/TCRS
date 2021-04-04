603c846b2a7dd055ae9b1f4971ce12430b97637f
istareatscreens <schar138@uottawa.ca>
2021-04-04 19:54:05 -0400

    Corrected SQL in GetPersonInfo function in data access

--- Traffic Citation and Reporting System/TCRS.Database/DataAccess/User/DataAccess.cs
+++ Traffic Citation and Reporting System/TCRS.Database/DataAccess/User/DataAccess.cs
@@ -53,8 +53,8 @@ namespace TCRS.Database
                 LEFT JOIN highway_patrol_officer ON highway_patrol_officer.person_id = p.person_id
                 LEFT JOIN municipal_officer ON municipal_officer.person_id = p.person_id
                 LEFT JOIN school_rep ON school_rep.person_id = p.person_id
-                LEFT JOIN municipality ON municipality.manager_id = p.person_id
-                LEFT JOIN police_dept ON police_dept.manager_id = p.person_id";
+                LEFT JOIN municipality ON municipality.munic_id = municipal_officer.munic_id
+                LEFT JOIN police_dept ON police_dept.police_dept_id = highway_patrol_officer.police_dept_id";
 
             using (IDbConnection connection = new MySqlConnection(connectionString))
             {
