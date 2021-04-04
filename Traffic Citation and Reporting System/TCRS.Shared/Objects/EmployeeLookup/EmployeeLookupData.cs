2ac207f7504ea5ce73a530aadcd511559a8ad78e
istareatscreens <schar138@uottawa.ca>
2021-04-04 16:26:57 -0400

    Setup parent class Employee to contain name information, and EmployeeLookupData as a subclass

--- Traffic Citation and Reporting System/TCRS.Shared/Objects/EmployeeLookup/EmployeeLookupData.cs
+++ Traffic Citation and Reporting System/TCRS.Shared/Objects/EmployeeLookup/EmployeeLookupData.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TCRS.Shared.Objects.EmployeeLookup
 {
-    public class EmployeeLookupData
+    public class EmployeeLookupData : Employee
     {
         // get object
         // person id is passed as parameter
         public int person_id { get; set; }
 
         // return that person's name, email, etc
-        public string first_name { get; set; }
-        public string last_name { get; set; }
         public string email { get; set; }
         public bool active { get; set; }
         public int police_dept_id { get; set; }
         public int munic_id { get; set; }
 
-
-        public string GetEmployeeName()
-        {
-            return first_name + " " + last_name;
-        }
 
         public IEnumerable<KeyValuePair<int, int>> CitationCountbyType { get; set; }
 
