b29c82b7b1c75870937d5dbba867086b287f23a5
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-04-05 21:14:45 -0400

    Front end changes for lookup portal warrant table, and warrant data object created

---
+++ Traffic Citation and Reporting System/TCRS.Shared/Objects/Warrant/WarrantData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCRS.Shared.Objects.Warrant
+{
+    class WarrantData
+    {
+        public int wanted_id;
+        public string reference_number;
+        // only returned
+        public string status;
+        public string crime;
+    }
+}
